fcd4e492e8e9c0caa0c2c53f006f5fdf7b3f28d6
TitanSnow <sweeto@live.cn>
2018-08-26 16:09:35 +0800

    ctrl form

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,36 @@
 <template>
-  <div id="app">
-    <p>Hello World</p>
+  <div class="hola-container">
+    <div class="hola-columns hola-card-stack">
+      <div class="hola-columns-item">
+        <div class="hola-card">
+          <form>
+            <p>
+              <label>
+                <span>Quality:</span>
+                <input type="range" min="0" max="1" step="0.01" v-model.number="level" class="hola-form-ctrl">
+                <input type="number" min="0" max="1" step="0.01" v-model.number="level" class="hola-form-ctrl">
+              </label>
+            </p>
+            <p>
+              <label>
+                <span>Scale:</span>
+                <input type="range" min="0" max="2" step="0.01" v-model.number="scale" class="hola-form-ctrl">
+                <input type="number" min="0" max="2" step="0.01" v-model.number="scale" class="hola-form-ctrl">
+              </label>
+            </p>
+            <p>
+              <label>
+                <span>Type:</span>
+                <select v-model="type" class="hola-form-ctrl">
+                  <option value="image/webp">WebP</option>
+                  <option value="image/jpeg">JPEG</option>
+                </select>
+              </label>
+            </p>
+          </form>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,5 +38,27 @@
 import { Component, Vue } from 'vue-property-decorator';
 
 @Component
-export default class App extends Vue {}
+export default class App extends Vue {
+  level = 0.92
+  scale = 1
+  type = 'image/webp'
+}
 </script>
+
+<style lang="stylus" scoped>
+form
+  > p
+    > label
+      display flex
+      > :first-child
+        display flex
+        align-items center
+        width 7ch
+        font-weight bold
+      > :last-child
+        width 5.5ch
+      > :nth-child(2)
+        flex-grow 99
+        flex-shrink 99
+        width 0
+</style>
