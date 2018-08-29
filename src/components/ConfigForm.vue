1048f1c796c77d78660b190e2fd674dc3eaa54c8
TitanSnow <sweeto@live.cn>
2018-08-29 13:58:17 +0800

    ResizeObserver

--- src/components/ConfigForm.vue
+++ src/components/ConfigForm.vue
@@ -1,16 +1,16 @@
 <template>
-  <form>
+  <form ref="form">
     <p>
       <label>
         <span>Quality:</span>
-        <input type="range" min="0" max="1" step="0.01" v-model.number="config.level" class="hola-form-ctrl" :disabled="!config.compress">
+        <input type="range" min="0" max="1" step="0.01" v-model.number="config.level" class="hola-form-ctrl" :disabled="!config.compress" v-if="!compact">
         <input type="number" min="0" max="1" step="0.01" v-model.number="config.level" class="hola-form-ctrl" :disabled="!config.compress">
       </label>
     </p>
     <p>
       <label>
         <span>Scale:</span>
-        <input type="range" min="0" max="2" step="0.01" v-model.number="config.scale" class="hola-form-ctrl">
+        <input type="range" min="0" max="2" step="0.01" v-model.number="config.scale" class="hola-form-ctrl" v-if="!compact">
         <input type="number" min="0" max="2" step="0.01" v-model.number="config.scale" class="hola-form-ctrl">
       </label>
     </p>
@@ -37,10 +37,31 @@ export interface Config {
   compress: boolean
 }
 
+declare class ResizeObserver {
+  constructor(callback: ResizeObserverCallback)
+  public observe(target: Element): void
+  public unobserve(target: Element): void
+  public disconnect(): void
+}
+declare type ResizeObserverCallback = (entries: ResizeObserverEntry[], observer: ResizeObserver) => void;
+declare interface ResizeObserverEntry {
+  readonly target: Element;
+  readonly contentRect: DOMRectReadOnly;
+};
+
 @Component
 export default class ConfigForm extends Vue {
   @Prop()
   private config!: Config
+  private compact = false
+  private mounted() {
+    if (ResizeObserver) {
+      const rzob = new ResizeObserver(entries => {
+        this.compact = entries[0].contentRect.width < 250
+      })
+      rzob.observe(this.$refs.form as Element)
+    }
+  }
 }
 </script>
 
