24cbd45f2a4b59958f6f37e8390849fe59ff0785
TitanSnow <sweeto@live.cn>
2018-09-08 22:36:55 +0800

    properly fix ff

--- src/App.vue
+++ src/App.vue
@@ -174,7 +174,7 @@ slider-thumb()
   appearance none
   height 1.5em
   width 12px
-  background-color #eee
+  background #eee
   box-shadow inset 0 0 0 0 #fff
   transition box-shadow .3s ease
   border-radius 0
